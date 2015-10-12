27725cc136dae02f1c91ee8dd0cfaca0a9bf633a
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2015-10-12 12:48:34 -0300

    Simplify main.c, renamed *.3dsx files
    boot1.3dsx->boot_1.3dsx
    emergency.3dsx->boot_2.3dsx

--- source/main.c
+++ source/main.c
@@ -9,17 +9,6 @@
 #include "boot.h"
 #include "scanner.h"
 
-#include <string.h>
-
-u8 sdmcCurrent = 0;
-u64 nextSdCheck = 0;
-
-u32 wifiStatus = 0;
-u8 batteryLevel = 5;
-u8 charging = 0;
-int rebootCounter;
-titleBrowser_s titleBrowser;
-
 // handled in main
 // doing it in main is preferred because execution ends in launching another 3dsx
 void __appInit()
@@ -30,7 +19,6 @@ void __appInit()
 void __appExit()
 {
 }
-
 
 int main()
 {
@@ -43,39 +31,23 @@ int main()
     acInit();
     ptmInit();
     titlesInit();
-    
-    
+
     // offset potential issues caused by homebrew that just ran
     aptOpenSession();
     APT_SetAppCpuTimeLimit(NULL, 0);
     aptCloseSession();
-    
-    FSUSER_IsSdmcDetected(NULL, &sdmcCurrent);
-    
-    nextSdCheck = osGetTime()+250;
-    srand(svcGetSystemTick());
-    rebootCounter=257;
-    
-    
-    
+
     char * execPath;
-    
+
     hidScanInput();
-    
+
     if (hidKeysDown()&KEY_R || hidKeysHeld()&KEY_R) {
-        execPath = "/emergency.3dsx";
+        execPath = "/boot_2.3dsx";
     }
     else {
-        execPath = "/boot1.3dsx";
+        execPath = "/boot_1.3dsx";
     }
-    
-    
-    int i, l=-1; for(i=0; execPath[i]; i++) if(execPath[i]=='/')l=i;
-    static menuEntry_s me;
-    initMenuEntry(&me, execPath, &execPath[l+1], execPath, "", NULL);
-    
-    scanMenuEntry(&me);
-    
+
     // cleanup whatever we have to cleanup
     titlesExit();
     ptmExit();
@@ -86,7 +58,6 @@ int main()
     closeSDArchive();
     aptExit();
     srvExit();
-    
-    
-    return bootApp(me.executablePath, &me.descriptor.executableMetadata);
+
+    return bootApp(execPath, NULL);
 }
